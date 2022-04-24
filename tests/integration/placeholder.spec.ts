08f38c11d823d99935af6b09a842e8c9ce16ac3e
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2022-04-24 10:49:26 +0300

    chore: replaced integration tests with placeholder

---
+++ tests/integration/placeholder.spec.ts
@@ -0,0 +1,9 @@
+describe('placeholder', () => {
+  it('should be a placeholder', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should know math', () => {
+    expect(1 + 1).toBe(2);
+  })
+})
