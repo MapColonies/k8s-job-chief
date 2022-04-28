18a554305923d6567e5a38f4fbd83c536a56e896
Daniel Hermon <danielhermonsync@gmail.com>
2022-04-28 15:09:16 +0300

    refactor: initialized server in index

--- src/common/liveness.ts
+++ src/common/liveness.ts
@@ -1,36 +1,7 @@
-import { createServer } from 'http';
-import { createTerminus } from '@godaddy/terminus';
 import { FactoryFunction } from 'tsyringe';
-import { Logger } from '@map-colonies/js-logger';
-import { ServerBuilder } from '../httpServer/serverBuilder';
-import { ShutdownHandler } from './shutdownHandler';
-import { IConfig, IServerConfig } from './interfaces';
-import { DEFAULT_SERVER_PORT, SERVICES } from './constants';
 
 const stubHealthcheck = async (): Promise<void> => Promise.resolve();
 
-export const livenessProbeFactory: FactoryFunction<void> = (container) => {
-  const config = container.resolve<IConfig>(SERVICES.CONFIG);
-  const serverConfig = config.get<IServerConfig>('server');
-  const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
-
-  const shutdownHandler = container.resolve(ShutdownHandler);
-
-  const app = container.resolve(ServerBuilder).build();
-  const server = createTerminus(createServer(app), {
-    healthChecks: { '/liveness': stubHealthcheck },
-    onSignal: shutdownHandler.onShutdown.bind(shutdownHandler),
-  });
-
-  shutdownHandler.addFunction(async () => {
-    return new Promise((resolve) => {
-      server.once('close', resolve);
-      server.close();
-    });
-  });
-
-  server.listen(port, () => {
-    const logger = container.resolve<Logger>(SERVICES.LOGGER);
-    logger.info(`app started on port ${port}`);
-  });
+export const livenessProbeFactory: FactoryFunction<void> = () => {
+  return stubHealthcheck;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 /* eslint-disable import/first */
 // this import must be called before the first import of tsyring
 import 'reflect-metadata';
+import { createServer } from 'http';
 import { Logger } from '@map-colonies/js-logger';
+import { createTerminus } from '@godaddy/terminus';
 import { DependencyContainer } from 'tsyringe';
-import { JOB_CLEANER_FACTORY, LIVENESS_PROBE_FACTORY, SERVICES } from './common/constants';
+import { JOB_CLEANER_FACTORY, LIVENESS_PROBE_FACTORY, DEFAULT_SERVER_PORT, SERVICES } from './common/constants';
 import { ShutdownHandler } from './common/shutdownHandler';
+import { ServerBuilder } from './httpServer/serverBuilder';
 import { registerExternalValues } from './containerConfig';
 import { JobsManager } from './manager/jobsManager';
+import { IConfig, IServerConfig } from './common/interfaces';
 
 let depContainer: DependencyContainer | undefined;
 
@@ -14,14 +18,34 @@ void registerExternalValues()
   .then(async (container) => {
     depContainer = container;
 
-    container.resolve<void>(LIVENESS_PROBE_FACTORY);
-
+    const livenessCheck = container.resolve<() => Promise<void>>(LIVENESS_PROBE_FACTORY);
     container.resolve<void>(JOB_CLEANER_FACTORY);
 
-    const manager = container.resolve(JobsManager);
+    const config = depContainer.resolve<IConfig>(SERVICES.CONFIG);
+    const serverConfig = config.get<IServerConfig>('server');
+    const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
+    const app = depContainer.resolve(ServerBuilder).build();
     const shutdownHandler = container.resolve(ShutdownHandler);
+    const server = createTerminus(createServer(app), {
+      healthChecks: { '/liveness': livenessCheck },
+      onSignal: shutdownHandler.onShutdown.bind(shutdownHandler),
+    });
+
+    shutdownHandler.addFunction(async () => {
+      return new Promise((resolve) => {
+        server.once('close', resolve);
+        server.close();
+      });
+    });
+
+    const manager = container.resolve(JobsManager);
     shutdownHandler.addFunction(manager.stop.bind(manager));
     await manager.start();
+
+    server.listen(port, () => {
+      const logger = container.resolve<Logger>(SERVICES.LOGGER);
+      logger.info(`app started on port ${port}`);
+    });
   })
   .catch(async (error: Error) => {
     const errorLogger =
